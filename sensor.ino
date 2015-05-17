5f4bf0c1234288320a319bdf9a628795f31dad34
Robert Daigle <crunchytortoise@Roberts-MacBook-Pro.local>
2015-05-17 18:42:24 -0400

    Adding the sensor.io file

---
+++ sensor.ino
@@ -0,0 +1,107 @@
+// This #include statement was automatically added by the Spark IDE.
+#include "HttpClient/HttpClient.h"
+
+/*
+HC-SR04 Ping distance sensor]
+VCC to arduino 5v GND to arduino GND
+Echo to Arduino pin 13 Trig to Arduino pin 12
+Red POS to Arduino pin 11
+Green POS to Arduino pin 10
+560 ohm resistor to both LED NEG and GRD power rail
+More info at: http://goo.gl/kJ8Gl
+Original code improvements to the Ping sketch sourced from Trollmaker.com
+Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+*/
+
+#define trigPin D0
+#define echoPin D1
+#define led D2
+#define led2 D3
+
+unsigned long pulseIn(uint16_t pin, uint8_t state) {
+    
+    GPIO_TypeDef* portMask = (PIN_MAP[pin].gpio_peripheral); // Cache the target's peripheral mask to speed up the loops.
+    uint16_t pinMask = (PIN_MAP[pin].gpio_pin); // Cache the target's GPIO pin mask to speed up the loops.
+    unsigned long pulseCount = 0; // Initialize the pulseCount variable now to save time.
+    unsigned long loopCount = 0; // Initialize the loopCount variable now to save time.
+    unsigned long loopMax = 20000000; // Roughly just under 10 seconds timeout to maintain the Spark Cloud connection.
+    
+    // Wait for the pin to enter target state while keeping track of the timeout.
+    while (GPIO_ReadInputDataBit(portMask, pinMask) != state) {
+        if (loopCount++ == loopMax) {
+            return 0;
+        }
+    }
+    
+    // Iterate the pulseCount variable each time through the loop to measure the pulse length; we also still keep track of the timeout.
+    while (GPIO_ReadInputDataBit(portMask, pinMask) == state) {
+        if (loopCount++ == loopMax) {
+            return 0;
+        }
+        pulseCount++;
+    }
+    
+    // Return the pulse time in microseconds by multiplying the pulseCount variable with the time it takes to run once through the loop.
+    return pulseCount * 0.405; // Calculated the pulseCount++ loop to be about 0.405uS in length.
+}
+
+HttpClient http;
+http_request_t request;
+http_response_t response;
+// Headers currently need to be set at init, useful for API keys etc.
+http_header_t headers[] = {
+    //  { "Content-Type", "application/json" },
+    //  { "Accept" , "application/json" },
+    { "Accept" , "*/*"},
+    { NULL, NULL } // NOTE: Always terminate headers will NULL
+};
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+void loop() {
+  long duration, distance;
+
+  
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;               //WRITE IN YOUR OWN DISTANCE
+  
+  if (distance < 110) {  // This is where the LED On/Off happens
+    digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+    digitalWrite(led2,LOW);
+
+    request.ip = {192,168,0,5};                 //WRITE IN YOUR IP
+    request.port = 3000;                        //WRITE IN YOUR PORT
+    request.path = "/";                         //WRITE IN YOUR PATH
+
+    // Get request
+    http.get(request, response, headers);
+    Serial.print("Application>\tResponse status: ");
+    Serial.println(response.status);
+    
+    Serial.print("Application>\tHTTP Response Body: ");
+    Serial.println(response.body);
+  }
+  else {
+    digitalWrite(led,LOW);
+    digitalWrite(led2,HIGH);
+  }
+  if (distance >= 200 || distance <= 0){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
+}
